3fed31f207594fb8949cf9c02f3beef76bd138ea
Dana Brash <dana.m.brash@gmail.com>
2025-04-23 14:04:10 -0700

    removing variable defaults

--- variables.tf
+++ variables.tf
@@ -2,23 +2,19 @@
 variable "projName" {
   description = "Short project name prefix"
   type        = string
-  default     = "cala"
 }
 
 variable "loc" {
   description = "Azure location (e.g. eastus2)"
   type        = string
-  default     = "westus2"
 }
 
 variable "type" {
   description = "resource type (e.g. SQL Server)"
   type        = string
-  default     = "vm"
 }
 
 variable "iterator" {
   description = "Incrementing counter for uniqueness"
   type        = string
-  default     = 1
 }
